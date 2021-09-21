799b815b8b3b466787964e55932297cdf7566e84
Bryce <bryce.cc.dunn@gmail.com>
2021-09-20 20:23:27 -0500

    tests and two bug fixes

--- src/Carousel.test.js
+++ src/Carousel.test.js
@@ -2,6 +2,15 @@ import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import Carousel from "./Carousel";
 
+it('should render without crashing', function() {
+    render(<Carousel/>)
+})
+
+it('should match snapshot', () => {
+  const { asFragment } = render(<Carousel/>);
+  expect(asFragment()).toMatchSnapshot();
+})
+
 it("works when you click on the right arrow", function() {
   const { queryByTestId, queryByAltText } = render(<Carousel />);
 
@@ -17,3 +26,60 @@ it("works when you click on the right arrow", function() {
   expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).not.toBeInTheDocument();
   expect(queryByAltText("Photo by Pratik Patel on Unsplash")).toBeInTheDocument();
 });
+
+it("works when you click on the left arrow", function() {
+  const { queryByTestId, queryByAltText } = render(<Carousel />);
+
+  // move forward in the carousel to start
+  const rightArrow = queryByTestId("right-arrow");
+  fireEvent.click(rightArrow);
+
+  // expect the second image to show, but not the first
+  expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).not.toBeInTheDocument();
+  expect(queryByAltText("Photo by Pratik Patel on Unsplash")).toBeInTheDocument();
+
+  // move backward in the carousel
+  const leftArrow = queryByTestId("left-arrow");
+  fireEvent.click(leftArrow);
+  
+  // expect the first image to show, but not the second
+  expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).toBeInTheDocument();
+  expect(queryByAltText("Photo by Pratik Patel on Unsplash")).not.toBeInTheDocument();
+});
+
+it('hides left arrow when on first image', () => {
+  const { queryByTestId, queryByAltText } = render(<Carousel/>);
+
+  // expect the first image to show, but not the second
+  expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).toBeInTheDocument();
+  expect(queryByAltText("Photo by Pratik Patel on Unsplash")).not.toBeInTheDocument();
+
+  // expect left arrow to not be visible
+  expect(queryByTestId("left-arrow")).not.toBeVisible();
+ 
+  // move forward in the carousel
+  const rightArrow = queryByTestId("right-arrow");
+  fireEvent.click(rightArrow);
+
+  // expect left arrow to be visible
+  expect(queryByTestId("left-arrow")).toBeVisible();
+})
+
+it('hides right arrow when on last image', () => {
+  const { queryByTestId, queryByAltText } = render(<Carousel/>);
+
+  // expect the first image to show, but not the second
+  expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).toBeInTheDocument();
+  expect(queryByAltText("Photo by Pratik Patel on Unsplash")).not.toBeInTheDocument();
+
+  // expect right arrow to  be visible
+  expect(queryByTestId("right-arrow")).toBeVisible();
+ 
+  // move forward in the carousel twice
+  const rightArrow = queryByTestId("right-arrow");
+  fireEvent.click(rightArrow);
+  fireEvent.click(rightArrow);
+
+  // expect right arrow to not be visible
+  expect(queryByTestId("right-arrow")).not.toBeVisible();
+})
